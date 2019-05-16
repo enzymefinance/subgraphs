91c61fa0aee6e129697a11e2bfd6204aec205aab
Ivan Herger <ivan.herger@gmail.com>
2019-05-16 19:58:14 +0200

    Policies

--- src/mappings/PolicyManager.ts
+++ src/mappings/PolicyManager.ts
@@ -1,5 +1,10 @@
 import { Registration } from "../types/PolicyManagerFactoryDataSource/templates/PolicyManagerDataSource/PolicyManagerContract";
+import { Policy } from "../types/schema";
 
 export function handleRegistration(event: Registration): void {
-  // TODO
+  // TODO (understand policymanager, policy and individual policies)
+  let policy = new Policy(event.transaction.hash.toHex());
+  policy.position = event.params.position;
+  policy.policy = event.params.policy.toHex();
+  policy.save();
 }
