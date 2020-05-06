d1971129bd572c0c5b5ac2146a1063ebbdbe4a0c
Sebastian Siemssen <fubhy@fubhy.com>
2020-05-06 14:14:32 +0200

    Add utility function for filtering unique items with a pluck fn.

--- src/utils/arrayUnique.ts
+++ src/utils/arrayUnique.ts
@@ -8,3 +8,15 @@ export function arrayUnique<T>(array: T[]): T[] {
 
   return unique;
 }
+
+export function arrayUniqueBy<T, U>(array: T[], pluck: (item: T) => U): T[] {
+  let references = array.map<U>((item) => pluck(item));
+  let unique: T[] = [];
+  for (let i: i32 = 0; i < references.length; i++) {
+    if (references.indexOf(references[i]) == i) {
+      unique.push(array[i]);
+    }
+  }
+
+  return unique;
+}
