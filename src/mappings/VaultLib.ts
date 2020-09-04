90abdfc987e42f8f3276201acddbe17f6ece9aa6
Erin Koen <erin.koen@gmail.com>
2020-09-04 16:48:34 -0400

    feat: write handleTrackedAssetAdded and handleTrackedAssetRemoved functions

--- src/mappings/VaultLib.ts
+++ src/mappings/VaultLib.ts
@@ -9,13 +9,39 @@ import {
   Transfer,
   VaultLibSet,
 } from '../generated/VaultLibContract';
+import { dataSource } from '@graphprotocol/graph-ts';
+import { useFund } from '../entities/Fund';
+import { ensureAsset } from '../entities/Asset';
+import { TrackedAssetAddition, TrackedAssetRemoval } from '../generated/schema';
+import { createContractEvent } from '../entities/Event';
 
 export function handleAccessorSet(event: AccessorSet): void {}
 export function handleApproval(event: Approval): void {}
 export function handleAssetWithdrawn(event: AssetWithdrawn): void {}
 export function handleMigratorSet(event: MigratorSet): void {}
 export function handleOwnerSet(event: OwnerSet): void {}
-export function handleTrackedAssetAdded(event: TrackedAssetAdded): void {}
-export function handleTrackedAssetRemoved(event: TrackedAssetRemoved): void {}
+
+export function handleTrackedAssetAdded(event: TrackedAssetAdded): void {
+  let id = event.transaction.hash.toHex() + '-' + event.logIndex.toString();
+  let trackedAssetAddition = new TrackedAssetAddition(id);
+  let fund = useFund(dataSource.context().getString('vaultProxy'));
+  trackedAssetAddition.asset = ensureAsset(event.params.asset).id;
+  trackedAssetAddition.fund = fund.id;
+  trackedAssetAddition.timestamp = event.block.timestamp;
+  trackedAssetAddition.save();
+  createContractEvent('TrackedAssetAdded', event);
+}
+
+export function handleTrackedAssetRemoved(event: TrackedAssetRemoved): void {
+  let id = event.transaction.hash.toHex() + '-' + event.logIndex.toString();
+  let trackedAssetRemoval = new TrackedAssetRemoval(id);
+  let fund = useFund(dataSource.context().getString('vaultProxy'));
+  trackedAssetRemoval.asset = ensureAsset(event.params.asset).id;
+  trackedAssetRemoval.fund = fund.id;
+  trackedAssetRemoval.timestamp = event.block.timestamp;
+  trackedAssetRemoval.save();
+  createContractEvent('TrackedAssetRemoved', event);
+}
+
 export function handleTransfer(event: Transfer): void {}
 export function handleVaultLibSet(event: VaultLibSet): void {}
