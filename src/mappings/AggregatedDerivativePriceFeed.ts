7cae202d5d429d2e113ed43dc5895011c1ff7210
Gabriel Rocheleau <gab_rocheleau@hotmail.com>
2020-09-09 19:05:13 -0400

    feat: handlePriceFeedSet mapping

--- src/mappings/AggregatedDerivativePriceFeed.ts
+++ src/mappings/AggregatedDerivativePriceFeed.ts
@@ -1,3 +1,36 @@
 import { PriceFeedSet } from '../generated/AggregatedDerivativePriceFeedContract';
+import { DerivativePriceFeedSet } from '../generated/schema';
+import { genericId } from '../utils/genericId';
+import { ensureAsset } from '../entities/Asset';
+import { ensureContract } from '../entities/Contract';
+import { ensurePriceFeed } from '../entities/PriceFeed';
+import { ensureTransaction } from '../entities/Transaction';
+import { zeroAddress } from '../constants';
 
-export function handlePriceFeedSet(event: PriceFeedSet): void {}
+export function handlePriceFeedSet(event: PriceFeedSet): void {
+  let derivativePriceFeedSet = new DerivativePriceFeedSet(genericId(event));
+
+  if (!event.params.prevPriceFeed.equals(zeroAddress)) {
+    let prevPriceFeed = ensurePriceFeed(event.params.prevPriceFeed);
+    prevPriceFeed.current = false;
+    prevPriceFeed.currentEnd = event.block.timestamp;
+    prevPriceFeed.save();
+
+    derivativePriceFeedSet.prevPriceFeed = prevPriceFeed.id;
+  }
+
+  let derivative = ensureAsset(event.params.derivative);
+
+  let nextPriceFeed = ensurePriceFeed(event.params.nextPriceFeed);
+  nextPriceFeed.current = true;
+  nextPriceFeed.currentStart = event.block.timestamp;
+  nextPriceFeed.save();
+
+  derivativePriceFeedSet.derivative = derivative.id;
+  derivativePriceFeedSet.nextPriceFeed = nextPriceFeed.id;
+  derivativePriceFeedSet.contract = ensureContract(event.address, 'DerivativePriceFeedSet', event.block.timestamp).id;
+  derivativePriceFeedSet.timestamp = event.block.timestamp;
+  derivativePriceFeedSet.transaction = ensureTransaction(event).id;
+
+  derivativePriceFeedSet.save();
+}
