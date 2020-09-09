b1ef76a5d1512ca8472064078ac7da42ff648123
Erin Koen <erin.koen@gmail.com>
2020-09-09 17:38:33 -0400

    feat: add policy mappings, draft policy handlers

--- src/mappings/PolicyManager.ts
+++ src/mappings/PolicyManager.ts
@@ -1,5 +1,41 @@
 import { PolicyDeregistered, PolicyEnabledForFund, PolicyRegistered } from '../generated/PolicyManagerContract';
+import { genericId } from '../utils/genericId';
+import { useFund } from '../entities/Fund';
+import { PolicyRegistration, PolicyDeregistration, PolicyEnabled } from '../generated/schema';
+import { ensureTransaction } from '../entities/Transaction';
+import { ensureContract } from '../entities/Contract';
+import { Address } from '@graphprotocol/graph-ts';
+import { ensureAccount } from '../entities/Account';
 
-export function handlePolicyDeregistered(event: PolicyDeregistered): void {}
-export function handlePolicyEnabledForFund(event: PolicyEnabledForFund): void {}
-export function handlePolicyRegistered(event: PolicyRegistered): void {}
+export function handlePolicyDeregistered(event: PolicyDeregistered): void {
+  let id = genericId(event);
+  let deregistration = new PolicyDeregistration(id);
+  deregistration.timestamp = event.block.timestamp;
+  deregistration.transaction = ensureTransaction(event).id;
+  deregistration.contract = ensureContract(event.params.policy, 'PolicyManager', event.block.timestamp).id;
+  // the protocol output for identifier is a string - not sure what to do with this
+  deregistration.policy = event.params.identifier;
+  deregistration.save();
+}
+
+export function handlePolicyEnabledForFund(event: PolicyEnabledForFund): void {
+  let id = genericId(event);
+  let enabled = new PolicyEnabled(id);
+  let fund = useFund(event.address.toHex());
+  enabled.fund = fund.id
+  enabled.account = ensureAccount(Address.fromString(fund.manager)).id
+  enabled.timestamp = event.block.timestamp
+  enabled.transaction = ensureTransaction(event).id;
+  enabled.policy = event.params.policy
+}
+
+export function handlePolicyRegistered(event: PolicyRegistered): void {
+  let id = genericId(event);
+  let registration = new PolicyRegistration(id);
+  registration.timestamp = event.block.timestamp;
+  registration.transaction = ensureTransaction(event).id;
+  registration.contract = ensureContract(event.address, 'PolicyManager', event.block.timestamp).id;
+  // see line 16
+  registration.policy = event.params.identifier
+  registration.save();
+}
