0e81834deb39d7d176b426f27e0880a21dcc24fd
Gabriel Rocheleau <gab_rocheleau@hotmail.com>
2020-09-09 19:04:53 -0400

    feat: ensurePriceFeed helper method

---
+++ src/entities/PriceFeed.ts
@@ -0,0 +1,15 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { PriceFeed } from '../generated/schema';
+
+export function ensurePriceFeed(priceFeedAddress: Address): PriceFeed {
+  let priceFeed = PriceFeed.load(priceFeedAddress.toHex()) as PriceFeed;
+  if (priceFeed) {
+    return priceFeed;
+  }
+
+  priceFeed = new PriceFeed(priceFeedAddress.toHex());
+  priceFeed.current = false;
+  priceFeed.save();
+
+  return priceFeed;
+}
