02ef3716efff51d605c2ea586b1c6ba1ed7cca94
Gabriel Rocheleau <gab_rocheleau@hotmail.com>
2020-09-10 14:07:06 -0400

    feat: WIP, adding pricefeed to Asset instance

--- src/mappings/AggregatedDerivativePriceFeed.ts
+++ src/mappings/AggregatedDerivativePriceFeed.ts
@@ -1,7 +1,7 @@
 import { PriceFeedSet } from '../generated/AggregatedDerivativePriceFeedContract';
 import { DerivativePriceFeedSet } from '../generated/schema';
 import { genericId } from '../utils/genericId';
-import { ensureAsset } from '../entities/Asset';
+import { useAsset, ensureAsset } from '../entities/Asset';
 import { ensureContract } from '../entities/Contract';
 import { ensurePriceFeed } from '../entities/PriceFeed';
 import { ensureTransaction } from '../entities/Transaction';
@@ -9,26 +9,34 @@ import { zeroAddress } from '../constants';
 
 export function handlePriceFeedSet(event: PriceFeedSet): void {
   let derivativePriceFeedSet = new DerivativePriceFeedSet(genericId(event));
+  let nextPriceFeed = ensurePriceFeed(event.params.nextPriceFeed);
+
   let derivative = ensureAsset(event.params.derivative);
+
+  // Specify that our Asset instance is a derivative
   derivative.isDerivative = true;
+
+  // TODO: Find out what the derivative is derived from & assign it
+  // primitive =
+  // derivative.derivedFrom =
+
+  // Assign our derivative its new price feed
+  derivative.currentPriceFeed = nextPriceFeed.id;
   derivative.save();
 
   if (!event.params.prevPriceFeed.equals(zeroAddress)) {
     let prevPriceFeed = ensurePriceFeed(event.params.prevPriceFeed);
-    prevPriceFeed.asset = derivative.id;
-    prevPriceFeed.current = false;
-    prevPriceFeed.currentEnd = event.block.timestamp;
+    // Check if the price feed is already listed for the asset
+
+    prevPriceFeed.asset.push(derivative.id);
+    /*     prevPriceFeed.current = false;
+    prevPriceFeed.currentEnd = event.block.timestamp; */
     prevPriceFeed.save();
 
     derivativePriceFeedSet.prevPriceFeed = prevPriceFeed.id;
   }
 
-  // TODO: Get underlying asset for derivative
-
-  let nextPriceFeed = ensurePriceFeed(event.params.nextPriceFeed);
-  nextPriceFeed.asset = derivative.id;
-  nextPriceFeed.current = true;
-  nextPriceFeed.currentStart = event.block.timestamp;
+  nextPriceFeed.asset.push(derivative);
   nextPriceFeed.save();
 
   derivativePriceFeedSet.derivative = derivative.id;
