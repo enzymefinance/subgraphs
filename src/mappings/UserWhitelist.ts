3f3ae10c8541fea0ec09c21a81255beaf38d9499
Ivan Herger <ivan.herger@gmail.com>
2020-09-24 08:15:51 +0200

    feat: add empty handler for UserWhitelist policy

---
+++ src/mappings/UserWhitelist.ts
@@ -0,0 +1,4 @@
+import { AddressesAdded, AddressesRemoved } from '../generated/UserWhitelistContract';
+
+export function handleAddressesAdded(event: AddressesAdded): void {}
+export function handleAddressesRemoved(event: AddressesRemoved): void {}
