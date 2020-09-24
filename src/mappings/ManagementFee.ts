8cfd3c644f42731d3362171ce9b33398d783ff27
Ivan Herger <ivan.herger@gmail.com>
2020-09-24 07:52:18 +0200

    feat: empty handlers for Performance Fee

--- src/mappings/ManagementFee.ts
+++ src/mappings/ManagementFee.ts
@@ -1,2 +1,4 @@
+import { FundSettingsAdded, Settled } from '../generated/ManagementFeeContract';
+
 export function handleFundSettingsAdded(event: FundSettingsAdded): void {}
 export function handleSettled(event: Settled): void {}
---
+++ src/mappings/PerformanceFee.ts
@@ -0,0 +1,6 @@
+import { ActivatedForFund, FundSettingsAdded, PaidOut, PerformanceUpdated } from '../generated/PerformanceFeeContract';
+
+export function handleActivatedForFund(event: ActivatedForFund): void {}
+export function handleFundSettingsAdded(event: FundSettingsAdded): void {}
+export function handlePaidOut(event: PaidOut): void {}
+export function handlePerformanceUpdated(event: PerformanceUpdated): void {}
