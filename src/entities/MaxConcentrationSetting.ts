aacbfe2c726106e363f3cbe226ada98fd3d21f17
Ivan Herger <ivan.herger@gmail.com>
2020-09-24 14:17:32 +0200

    feat: handlers and entities for MaxConcentration policy

---
+++ src/entities/MaxConcentrationSetting.ts
@@ -0,0 +1,37 @@
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
+import { MaxConcentrationSetting, Fund, Policy } from '../generated/schema';
+import { logCritical } from '../utils/logCritical';
+
+export function maxConcentrationSettingId(fundId: string, policy: Policy): string {
+  return fundId + '/' + policy.id;
+}
+
+export function useMaxConcentrationSetting(fund: Fund, policy: Policy): MaxConcentrationSetting {
+  let id = maxConcentrationSettingId(fund.id, policy);
+  let setting = MaxConcentrationSetting.load(id) as MaxConcentrationSetting;
+
+  if (setting == null) {
+    logCritical('Failed to load MaxConcentrationSetting {}.', [id]);
+  }
+
+  return setting as MaxConcentrationSetting;
+}
+
+export function ensureMaxConcentrationSetting(fundId: string, policy: Policy): MaxConcentrationSetting {
+  let id = maxConcentrationSettingId(fundId, policy);
+  let setting = MaxConcentrationSetting.load(id) as MaxConcentrationSetting;
+
+  if (setting) {
+    return setting;
+  }
+
+  setting = new MaxConcentrationSetting(id);
+  setting.policy = policy.id;
+  setting.fund = fundId;
+  setting.maxConcentration = BigDecimal.fromString('0');
+  setting.events = [];
+  setting.timestamp = BigInt.fromI32(0);
+  setting.save();
+
+  return setting;
+}
--- src/mappings/MaxConcentration.ts
+++ src/mappings/MaxConcentration.ts
@@ -1,3 +1,35 @@
+import { useManager } from '../entities/Account';
+import { useComptroller } from '../entities/Comptroller';
+import { ensureContract } from '../entities/Contract';
+import { ensureMaxConcentrationSetting } from '../entities/MaxConcentrationSetting';
+import { usePolicy } from '../entities/Policy';
+import { ensureTransaction } from '../entities/Transaction';
+import { ComptrollerLibContract } from '../generated/ComptrollerLibContract';
 import { MaxConcentrationSet } from '../generated/MaxConcentrationContract';
+import { MaxConcentrationSetEvent } from '../generated/schema';
+import { arrayUnique } from '../utils/arrayUnique';
+import { genericId } from '../utils/genericId';
+import { toBigDecimal } from '../utils/tokenValue';
 
-export function handleMaxConcentrationSet(event: MaxConcentrationSet): void {}
+export function handleMaxConcentrationSet(event: MaxConcentrationSet): void {
+  let comptroller = ComptrollerLibContract.bind(event.params.comptrollerProxy);
+  let vault = comptroller.getVaultProxy();
+  let policy = usePolicy(event.address.toHex());
+  let value = toBigDecimal(event.params.value);
+
+  let maxConcentration = new MaxConcentrationSetEvent(genericId(event));
+  maxConcentration.fund = vault.toHex(); // fund does not exist yet
+  maxConcentration.account = useManager(event.transaction.from.toHex()).id;
+  maxConcentration.contract = ensureContract(event.address, 'MaxConcentration', event).id;
+  maxConcentration.timestamp = event.block.timestamp;
+  maxConcentration.transaction = ensureTransaction(event).id;
+  maxConcentration.comptrollerProxy = useComptroller(event.params.comptrollerProxy.toHex()).id;
+  maxConcentration.value = value;
+  maxConcentration.save();
+
+  let setting = ensureMaxConcentrationSetting(vault.toHex(), policy);
+  setting.maxConcentration = value;
+  setting.events = arrayUnique<string>(setting.events.concat([maxConcentration.id]));
+  setting.timestamp = event.block.timestamp;
+  setting.save();
+}
