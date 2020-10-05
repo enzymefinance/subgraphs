9a8cd6557bb0370c44a9a708e0f909f0661bf78e
Sebastian Siemssen <fubhy@fubhy.com>
2020-10-05 19:26:31 +0200

    feat: use proxy

--- src/mappings/ChainlinkPriceFeed.ts
+++ src/mappings/ChainlinkPriceFeed.ts
@@ -12,7 +12,18 @@ import { arrayUnique } from '../utils/arrayUnique';
 import { ChainlinkAggregatorContract } from '../generated/ChainlinkAggregatorContract';
 import { trackAssetPrice } from '../entities/AssetPrice';
 import { toBigDecimal } from '../utils/toBigDecimal';
+import { ChainlinkAggregatorProxyContract } from '../generated/ChainlinkAggregatorProxyContract';
 
+// TODO: Chainlink uses proxies for their price oracles. Sadly, these proxies do not
+// emit events whenever the underlying aggregator changes. Chainlink has proposed that
+// they could add a central registry that emits an event whenever an aggregator changes.
+//
+// If they can't make this happen, an alternative solution could be to monitor these
+// changes (through contract calls) either as part of cron or by checking and comparing
+// the current aggregator whenever we track an AnswerUpdated event and swap out the
+// underlying aggregator whenever we observe a change. This would be possible because
+// chainlink (allegedly) keeps updating the "old" aggregator for a limited amount of time
+// even after they've migrated to a new one and pointed the proxy to the new aggregator.
 export function handleAggregatorSet(event: AggregatorSet): void {
   let primitive = ensureAsset(event.params.primitive);
 
@@ -26,17 +37,23 @@ export function handleAggregatorSet(event: AggregatorSet): void {
   primitivePriceFeedSet.save();
 
   if (!event.params.prevAggregator.equals(zeroAddress)) {
-    disableChainlinkAggregator(event.params.prevAggregator, primitive);
+    let proxy = ChainlinkAggregatorProxyContract.bind(event.params.prevAggregator);
+    let aggregator = proxy.aggregator();
+
+    disableChainlinkAggregator(aggregator, primitive);
   }
 
   if (!event.params.nextAggregator.equals(zeroAddress)) {
+    let proxy = ChainlinkAggregatorProxyContract.bind(event.params.nextAggregator);
+    let aggregator = proxy.aggregator();
+
     // Whenever a new asset is registered, we need to fetch its current price immediately.
-    let contract = ChainlinkAggregatorContract.bind(event.params.nextAggregator);
+    let contract = ChainlinkAggregatorContract.bind(aggregator);
     let current = toBigDecimal(contract.latestAnswer(), primitive.decimals);
     trackAssetPrice(primitive, event.block.timestamp, current);
 
     // Keep tracking this asset using the registered chainlink aggregator.
-    enableChainlinkAggregator(event.params.nextAggregator, primitive);
+    enableChainlinkAggregator(aggregator, primitive);
   }
 
   let cron = ensureCron();
