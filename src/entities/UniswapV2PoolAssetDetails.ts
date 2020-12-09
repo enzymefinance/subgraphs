64e0c48a2250307e2764352125474288813a258b
Ivan Herger <ivan.herger@gmail.com>
2020-12-09 10:01:30 +0100

    chore: rename UniswapPool

--- src/entities/UniswapV2PoolAssetDetails.ts
+++ src/entities/UniswapV2PoolAssetDetails.ts
@@ -1,4 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts';
+import { ERC20Contract } from '../generated/ERC20Contract';
 import { IUniswapV2Pair } from '../generated/IUniswapV2Pair';
 import { Asset, UniswapV2PoolAssetDetail } from '../generated/schema';
 
@@ -10,11 +11,23 @@ export function checkUniswapV2PoolAssetDetail(derivative: Asset): void {
   let uniswapPair = IUniswapV2Pair.bind(Address.fromString(derivative.id));
 
   let details = new UniswapV2PoolAssetDetail(derivative.id);
-  details.token0 = uniswapPair.token0().toHex();
-  details.token1 = uniswapPair.token1().toHex();
+  let token0Address = uniswapPair.token0();
+  let token1Address = uniswapPair.token1();
+
+  details.token0 = token0Address.toHex();
+  details.token1 = token1Address.toHex();
   details.save();
 
-  derivative.derivativeType = 'UniswapV2Pool';
+  let contract0 = ERC20Contract.bind(token0Address);
+  let contract1 = ERC20Contract.bind(token1Address);
+
+  let symbol0 = contract0.symbol();
+  let symbol1 = contract1.symbol();
+
+  let name = 'Uniswap ' + symbol0 + '/' + symbol1 + ' Pool';
+
+  derivative.name = name;
+  derivative.derivativeType = 'UniswapPool';
   derivative.uniswapV2PoolAssetDetail = details.id;
   derivative.save();
 }
