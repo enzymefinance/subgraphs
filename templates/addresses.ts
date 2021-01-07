7b4f7d6541e6f151d16508532e08767579f7d6c4
Sebastian Siemssen <fubhy@fubhy.com>
2021-01-07 17:44:30 +0100

    chore: re-add static address exports

--- templates/addresses.ts
+++ templates/addresses.ts
@@ -7,8 +7,33 @@ import { Address } from '@graphprotocol/graph-ts';
 // there are constant values both by convention but also simply through the
 // way we deploy our protocol, we can use these addresses here.
 export let wethTokenAddress = Address.fromString('{{wethToken}}');
+export let chaiPriceSourceAddress = Address.fromString('{{chaiPriceSource}}');
+export let chaiIntegrateeAddress = Address.fromString('{{chaiIntegratee}}');
+export let kyberIntegrateeAddress = Address.fromString('{{kyberIntegratee}}');
+export let dispatcherAddress = Address.fromString('{{dispatcher}}');
+export let vaultLibAddress = Address.fromString('{{vaultLib}}');
+export let fundDeployerAddress = Address.fromString('{{fundDeployer}}');
 export let valueInterpreterAddress = Address.fromString('{{valueInterpreter}}');
+export let comptrollerLibAddress = Address.fromString('{{comptrollerLib}}');
 export let fundActionsWrapperAddress = Address.fromString('{{fundActionsWrapper}}');
+export let feeManagerAddress = Address.fromString('{{feeManager}}');
+export let integrationManagerAddress = Address.fromString('{{integrationManager}}');
+export let policyManagerAddress = Address.fromString('{{policyManager}}');
+export let chainlinkPriceFeedAddress = Address.fromString('{{chainlinkPriceFeed}}');
+export let chaiPriceFeedAddress = Address.fromString('{{chaiPriceFeed}}');
+export let aggregatedDerivativePriceFeedAddress = Address.fromString('{{aggregatedDerivativePriceFeed}}');
+export let chaiAdapterAddress = Address.fromString('{{chaiAdapter}}');
+export let kyberAdapterAddress = Address.fromString('{{kyberAdapter}}');
+export let managementFeeAddress = Address.fromString('{{managementFee}}');
+export let performanceFeeAddress = Address.fromString('{{performanceFee}}');
+export let adapterBlacklistAddress = Address.fromString('{{adapterBlacklist}}');
+export let adapterWhitelistAddress = Address.fromString('{{adapterWhitelist}}');
+export let assetBlacklistAddress = Address.fromString('{{assetBlacklist}}');
+export let assetWhitelistAddress = Address.fromString('{{assetWhitelist}}');
+export let maxConcentrationAddress = Address.fromString('{{maxConcentration}}');
+export let investorWhitelistAddress = Address.fromString('{{investorWhitelist}}');
+export let buySharesCallerWhitelist = Address.fromString('{{buySharesCallerWhitelist}}');
+export let guaranteedRedemption = Address.fromString('{{guaranteedRedemption}}');
 
 export let audChainlinkAggregator = Address.fromString('{{audChainlinkAggregator}}');
 export let btcChainlinkAggregator = Address.fromString('{{btcChainlinkAggregator}}');
