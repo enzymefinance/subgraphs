1dd615f898255fce8168a5c67c00f45b3e664264
Ivan Herger <36133712+iherger@users.noreply.github.com>
2021-03-15 08:52:17 +0100

    feat: add pricefeeds to release (#33)

--- src/entities/Release.ts
+++ src/entities/Release.ts
@@ -1,9 +1,12 @@
 import { Address, ethereum } from '@graphprotocol/graph-ts';
 import {
   aaveAdapterAddress,
+  aavePriceFeed,
   adapterBlacklistAddress,
   adapterWhitelistAddress,
   aggregatedDerivativePriceFeedAddress,
+  alphaHomoraV1AdapterAddress,
+  alphaHomoraV1PriceFeed,
   assetBlacklistAddress,
   assetWhitelistAddress,
   authUserExecutedSharesRequestorFactoryAddress,
@@ -11,8 +14,11 @@ import {
   chaiAdapterAddress,
   chaiIntegrateeAddress,
   chainlinkPriceFeedAddress,
+  chaiPriceFeed,
   compoundAdapterAddress,
+  compoundPriceFeed,
   comptrollerLibAddress,
+  curvePriceFeed,
   dispatcherAddress,
   entranceRateBurnFeeAddress,
   entranceRateDirectFeeAddress,
@@ -24,24 +30,28 @@ import {
   investorWhitelistAddress,
   kyberAdapterAddress,
   kyberIntegrateeAddress,
+  lidoStethPriceFeed,
   managementFeeAddress,
   maxConcentrationAddress,
   minMaxInvestmentAddress,
   paraSwapAdapterAddress,
   performanceFeeAddress,
   policyManagerAddress,
+  stakehoundEthPriceFeed,
   synthetixAdapterAddress,
   synthetixAddressResolverAddress,
   synthetixDelegateApprovalsAddress,
   synthetixIntegrateeAddress,
+  synthetixPriceFeed,
   trackedAssetsAdapterAddress,
   uniswapV2AdapterAddress,
   uniswapV2IntegrateeAddress,
+  uniswapV2PoolPriceFeed,
   valueInterpreterAddress,
   vaultLibAddress,
+  wdgldPriceFeed,
   wethTokenAddress,
   zeroExV2AdapterAddress,
-  alphaHomoraV1AdapterAddress,
 } from '../addresses';
 import { Release } from '../generated/schema';
 import { logCritical } from '../utils/logCritical';
@@ -62,10 +72,12 @@ export function createRelease(address: Address, event: ethereum.Event): Release 
   release.open = event.block.timestamp;
   release.network = networkId;
 
+  release.aavePriceFeed = aavePriceFeed.toHex();
   release.adapterBlacklist = adapterBlacklistAddress.toHex();
   release.adapterWhitelist = adapterWhitelistAddress.toHex();
   release.aggregatedDerivativePriceFeed = aggregatedDerivativePriceFeedAddress.toHex();
   release.alphaHomoraV1Adapter = alphaHomoraV1AdapterAddress.toHex();
+  release.alphaHomoraV1PriceFeed = alphaHomoraV1PriceFeed.toHex();
   release.aaveAdapter = aaveAdapterAddress.toHex();
   release.assetBlacklist = assetBlacklistAddress.toHex();
   release.assetWhitelist = assetWhitelistAddress.toHex();
@@ -74,8 +86,11 @@ export function createRelease(address: Address, event: ethereum.Event): Release 
   release.chaiAdapter = chaiAdapterAddress.toHex();
   release.chaiIntegratee = chaiIntegrateeAddress.toHex();
   release.chainlinkPriceFeed = chainlinkPriceFeedAddress.toHex();
+  release.chaiPriceFeed = chaiPriceFeed.toHex();
   release.compoundAdapter = compoundAdapterAddress.toHex();
+  release.compoundPriceFeed = compoundPriceFeed.toHex();
   release.comptrollerLib = comptrollerLibAddress.toHex();
+  release.curvePriceFeed = curvePriceFeed.toHex();
   release.dispatcher = dispatcherAddress.toHex();
   release.entranceRateBurnFee = entranceRateBurnFeeAddress.toHex();
   release.entranceRateDirectFee = entranceRateDirectFeeAddress.toHex();
@@ -87,21 +102,26 @@ export function createRelease(address: Address, event: ethereum.Event): Release 
   release.investorWhitelist = investorWhitelistAddress.toHex();
   release.kyberAdapter = kyberAdapterAddress.toHex();
   release.kyberIntegratee = kyberIntegrateeAddress.toHex();
+  release.lidoStethPriceFeed = lidoStethPriceFeed.toHex();
   release.managementFee = managementFeeAddress.toHex();
   release.maxConcentration = maxConcentrationAddress.toHex();
   release.minMaxInvestment = minMaxInvestmentAddress.toHex();
   release.paraSwapAdapter = paraSwapAdapterAddress.toHex();
   release.performanceFee = performanceFeeAddress.toHex();
   release.policyManager = policyManagerAddress.toHex();
+  release.stakehoundEthPriceFeed = stakehoundEthPriceFeed.toHex();
   release.synthetixAdapter = synthetixAdapterAddress.toHex();
   release.synthetixAddressResolver = synthetixAddressResolverAddress.toHex();
   release.synthetixDelegateApprovals = synthetixDelegateApprovalsAddress.toHex();
   release.synthetixIntegratee = synthetixIntegrateeAddress.toHex();
+  release.synthetixPriceFeed = synthetixPriceFeed.toHex();
   release.trackedAssetsAdapter = trackedAssetsAdapterAddress.toHex();
   release.uniswapV2Adapter = uniswapV2AdapterAddress.toHex();
   release.uniswapV2Integratee = uniswapV2IntegrateeAddress.toHex();
+  release.uniswapV2PoolPriceFeed = uniswapV2PoolPriceFeed.toHex();
   release.valueInterpreter = valueInterpreterAddress.toHex();
   release.vaultLib = vaultLibAddress.toHex();
+  release.wdgldPriceFeed = wdgldPriceFeed.toHex();
   release.wethToken = wethTokenAddress.toHex();
   release.zeroExV2Adapter = zeroExV2AdapterAddress.toHex();
   release.save();
