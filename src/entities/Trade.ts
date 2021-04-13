dd8e830a96893a93e5d671795d6d922f8dbc5432
Ivan Herger <ivan.herger@gmail.com>
2021-04-13 07:36:00 +0200

    feat: add additional trade types

--- src/entities/Trade.ts
+++ src/entities/Trade.ts
@@ -1,23 +1,39 @@
 import { CallOnIntegrationExecutedForFund } from '../generated/IntegrationManagerContract';
 import {
   AddTrackedAssetsTrade,
+  ApproveAssetsTrade,
   AssetAmount,
+  ClaimRewardsAndReinvestTrade,
+  ClaimRewardsAndSwapTrade,
+  ClaimRewardsTrade,
   Fund,
   IntegrationAdapter,
+  LendAndStakeTrade,
   LendTrade,
   MultiLendTrade,
   MultiRedeemTrade,
   MultiTokenSwapTrade,
   RedeemTrade,
+  StakeTrade,
   TokenSwapTrade,
+  UnstakeAndRedeemTrade,
+  UnstakeTrade,
 } from '../generated/schema';
 import { genericId } from '../utils/genericId';
 import {
   addTrackedAssetsType,
+  approveAssetsType,
+  claimRewardsAndReinvestType,
+  claimRewardsAndSwapType,
+  claimRewardsType,
   convertSelectorToType,
+  lendAndStakeType,
   lendType,
   redeemType,
+  stakeType,
   takeOrderType,
+  unstakeAndRedeemType,
+  unstakeType,
 } from '../utils/integrationSelectors';
 
 export function trackTrade(
@@ -125,6 +141,116 @@ export function trackTrade(
     trade.adapter = adapter.id;
     trade.method = tradeType;
     trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == approveAssetsType) {
+    let trade = new ApproveAssetsTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == stakeType) {
+    let trade = new StakeTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmount = incomingAssetAmounts[0].id;
+    trade.outgoingAssetAmount = outgoingAssetAmounts[0].id;
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == unstakeType) {
+    let trade = new UnstakeTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmount = incomingAssetAmounts[0].id;
+    trade.outgoingAssetAmount = outgoingAssetAmounts[0].id;
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == claimRewardsType) {
+    let trade = new ClaimRewardsTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == claimRewardsAndReinvestType) {
+    let trade = new ClaimRewardsAndReinvestTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.outgoingAssetAmount = outgoingAssetAmounts[0].id;
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == claimRewardsAndSwapType) {
+    let trade = new ClaimRewardsAndSwapTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.outgoingAssetAmount = outgoingAssetAmounts[0].id;
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == lendAndStakeType) {
+    let trade = new LendAndStakeTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmounts = incomingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
+    trade.outgoingAssetAmount = outgoingAssetAmounts[0].id;
+    trade.timestamp = event.block.timestamp;
+    trade.fundState = fund.state;
+    trade.save();
+
+    return;
+  }
+
+  if (tradeType == unstakeAndRedeemType) {
+    let trade = new UnstakeAndRedeemTrade(genericId(event));
+    trade.fund = fund.id;
+    trade.adapter = adapter.id;
+    trade.method = tradeType;
+    trade.incomingAssetAmount = incomingAssetAmounts[0].id;
+    trade.outgoingAssetAmounts = outgoingAssetAmounts.map<string>((assetAmount) => assetAmount.id);
     trade.timestamp = event.block.timestamp;
     trade.fundState = fund.state;
     trade.save();
