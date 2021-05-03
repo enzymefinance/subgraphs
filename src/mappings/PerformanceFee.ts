61b5beb32bac0ea8d15aafae7947f8b72e40d9d6
Ivan Herger <ivan.herger@gmail.com>
2021-05-03 16:16:16 +0200

    fix: fix decimals for Performance Fee states

--- src/mappings/PerformanceFee.ts
+++ src/mappings/PerformanceFee.ts
@@ -1,4 +1,5 @@
-import { BigDecimal } from '@graphprotocol/graph-ts';
+import { Address, BigDecimal } from '@graphprotocol/graph-ts';
+import { ensureAsset } from '../entities/Asset';
 import { useFee } from '../entities/Fee';
 import { trackFeeState } from '../entities/FeeState';
 import { useFund } from '../entities/Fund';
@@ -48,16 +49,19 @@ export function handleFundSettingsAdded(event: FundSettingsAdded): void {
 }
 
 export function handleActivatedForFund(event: ActivatedForFund): void {
+  // fund entity does not yet exist, so we have to do contract calls
   let comptroller = ComptrollerLibContract.bind(event.params.comptrollerProxy);
   let vault = comptroller.getVaultProxy();
   let fee = useFee(event.address.toHex());
 
+  let denominationAsset = ensureAsset(comptroller.getDenominationAsset());
+
   let feeActivation = new PerformanceFeeActivatedForFundEvent(genericId(event));
-  feeActivation.fund = vault.toHex(); // fund does not exist yet
+  feeActivation.fund = vault.toHex();
   feeActivation.timestamp = event.block.timestamp;
   feeActivation.transaction = ensureTransaction(event).id;
   feeActivation.comptrollerProxy = event.params.comptrollerProxy.toHex();
-  feeActivation.highWaterMark = toBigDecimal(event.params.highWaterMark);
+  feeActivation.highWaterMark = toBigDecimal(event.params.highWaterMark, denominationAsset.decimals);
   feeActivation.save();
 
   let setting = ensurePerformanceFeeSetting(vault.toHex(), fee);
@@ -69,20 +73,21 @@ export function handleLastSharePriceUpdated(event: LastSharePriceUpdated): void 
   let comptroller = ComptrollerLibContract.bind(event.params.comptrollerProxy);
   let fund = useFund(comptroller.getVaultProxy().toHex());
   let fee = useFee(event.address.toHex());
+  let denominationAsset = ensureAsset(Address.fromString(fund.denominationAsset));
 
   let sharePriceUpdate = new PerformanceFeeSharePriceUpdatedEvent(genericId(event));
   sharePriceUpdate.fund = fund.id;
   sharePriceUpdate.timestamp = event.block.timestamp;
   sharePriceUpdate.transaction = ensureTransaction(event).id;
   sharePriceUpdate.comptrollerProxy = event.params.comptrollerProxy.toHex();
-  sharePriceUpdate.prevSharePrice = toBigDecimal(event.params.prevSharePrice);
-  sharePriceUpdate.nextSharePrice = toBigDecimal(event.params.nextSharePrice);
+  sharePriceUpdate.prevSharePrice = toBigDecimal(event.params.prevSharePrice, denominationAsset.decimals);
+  sharePriceUpdate.nextSharePrice = toBigDecimal(event.params.nextSharePrice, denominationAsset.decimals);
   sharePriceUpdate.save();
 
   trackFeeState(fund, fee, BigDecimal.fromString('0'), event, sharePriceUpdate);
 
   let performanceFeeState = usePerformanceFeeState(performanceFeeStateId(fund, event));
-  performanceFeeState.grossSharePrice = toBigDecimal(event.params.nextSharePrice);
+  performanceFeeState.grossSharePrice = toBigDecimal(event.params.nextSharePrice, denominationAsset.decimals);
   performanceFeeState.lastPaid = event.block.timestamp;
   performanceFeeState.save();
 }
@@ -91,20 +96,21 @@ export function handlePaidOut(event: PaidOut): void {
   let comptroller = ComptrollerLibContract.bind(event.params.comptrollerProxy);
   let fund = useFund(comptroller.getVaultProxy().toHex());
   let fee = useFee(event.address.toHex());
+  let denominationAsset = ensureAsset(Address.fromString(fund.denominationAsset));
 
   let paidOut = new PerformanceFeePaidOutEvent(genericId(event));
   paidOut.fund = fund.id;
   paidOut.timestamp = event.block.timestamp;
   paidOut.transaction = ensureTransaction(event).id;
   paidOut.comptrollerProxy = event.params.comptrollerProxy.toHex();
-  paidOut.prevHighWaterMark = toBigDecimal(event.params.prevHighWaterMark);
-  paidOut.nextHighWaterMark = toBigDecimal(event.params.nextHighWaterMark);
+  paidOut.prevHighWaterMark = toBigDecimal(event.params.prevHighWaterMark, denominationAsset.decimals);
+  paidOut.nextHighWaterMark = toBigDecimal(event.params.nextHighWaterMark, denominationAsset.decimals);
   paidOut.save();
 
   trackFeeState(fund, fee, BigDecimal.fromString('0'), event, paidOut);
 
   let performanceFeeState = usePerformanceFeeState(performanceFeeStateId(fund, event));
-  performanceFeeState.highWaterMark = toBigDecimal(event.params.nextHighWaterMark);
+  performanceFeeState.highWaterMark = toBigDecimal(event.params.nextHighWaterMark, denominationAsset.decimals);
   performanceFeeState.lastPaid = event.block.timestamp;
   performanceFeeState.save();
 }
@@ -113,21 +119,22 @@ export function handlePerformanceUpdated(event: PerformanceUpdated): void {
   let comptroller = ComptrollerLibContract.bind(event.params.comptrollerProxy);
   let fund = useFund(comptroller.getVaultProxy().toHex());
   let fee = useFee(event.address.toHex());
+  let denominationAsset = ensureAsset(Address.fromString(fund.denominationAsset));
 
   let updated = new PerformanceFeePerformanceUpdatedEvent(genericId(event));
   updated.fund = fund.id;
   updated.timestamp = event.block.timestamp;
   updated.transaction = ensureTransaction(event).id;
   updated.comptrollerProxy = event.params.comptrollerProxy.toHex();
-  updated.prevAggregateValueDue = toBigDecimal(event.params.prevAggregateValueDue);
-  updated.nextAggregateValueDue = toBigDecimal(event.params.nextAggregateValueDue);
+  updated.prevAggregateValueDue = toBigDecimal(event.params.prevAggregateValueDue, denominationAsset.decimals);
+  updated.nextAggregateValueDue = toBigDecimal(event.params.nextAggregateValueDue, denominationAsset.decimals);
   updated.sharesOutstandingDiff = toBigDecimal(event.params.sharesOutstandingDiff);
   updated.save();
 
   trackFeeState(fund, fee, BigDecimal.fromString('0'), event, updated);
 
   let performanceFeeState = usePerformanceFeeState(performanceFeeStateId(fund, event));
-  performanceFeeState.aggregateValueDue = toBigDecimal(event.params.nextAggregateValueDue);
+  performanceFeeState.aggregateValueDue = toBigDecimal(event.params.nextAggregateValueDue, denominationAsset.decimals);
   performanceFeeState.sharesOutstanding = performanceFeeState.sharesOutstanding.plus(
     toBigDecimal(event.params.sharesOutstandingDiff),
   );
