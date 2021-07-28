a44fe54c3bf39dd90542fded21f5470f8eec3241
Sebastian Siemssen <fubhy@fubhy.com>
2021-07-28 11:49:54 +0200

    feat: spawn vault proxy data source

--- subgraphs/vault-balances/mappings/Dispatcher.ts
+++ subgraphs/vault-balances/mappings/Dispatcher.ts
@@ -1,8 +1,18 @@
 import { VaultProxyDeployed } from '../generated/DispatcherContract';
 import { Vault } from '../generated/schema';
+import { VaultDataSource } from '../generated/templates';
 
 export function handleVaultProxyDeployed(event: VaultProxyDeployed): void {
   let id = event.params.vaultProxy.toHex();
+
+  // This should never happen but it doesn't hurt to check it nonetheless.
+  if (Vault.load(id) != null) {
+    return;
+  }
+
   let vault = new Vault(id);
   vault.save();
+
+  // Spawn the data source for the vault proxy to observe tracked assets and external positions.
+  VaultDataSource.create(event.params.vaultProxy);
 }
