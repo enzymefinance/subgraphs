4a96773936328896e265ab125183a92a5e5c7c40
Sebastian Siemssen <fubhy@fubhy.com>
2021-08-01 12:18:00 +0200

    chore: optimize transfer event tracking start block

--- subgraphs/vault-balances/subgraph.config.ts
+++ subgraphs/vault-balances/subgraph.config.ts
@@ -1,8 +1,9 @@
 import { Configurator, Contexts } from '@enzymefinance/subgraph-cli';
 
 interface Variables {
-  block: number;
   dispatcher: string;
+  dispatcherBlock: number;
+  firstVaultBlock: number;
 }
 
 const name = 'enzymefinance/vault-balances';
@@ -13,7 +14,8 @@ export const contexts: Contexts<Variables> = {
     network: 'kovan',
     variables: {
       dispatcher: '0xba9493530494958EC2DeED9c8BB34004ff37Ad28',
-      block: 24710049,
+      dispatcherBlock: 24710049,
+      firstVaultBlock: 24710049,
     },
   },
   mainnet: {
@@ -21,7 +23,8 @@ export const contexts: Contexts<Variables> = {
     network: 'mainnet',
     variables: {
       dispatcher: '0xC3DC853dD716bd5754f421ef94fdCbac3902ab32',
-      block: 11636493,
+      dispatcherBlock: 11636493,
+      firstVaultBlock: 11681281,
     },
   },
   local: {
@@ -30,7 +33,8 @@ export const contexts: Contexts<Variables> = {
     network: 'kovan',
     variables: {
       dispatcher: '0xba9493530494958EC2DeED9c8BB34004ff37Ad28',
-      block: 24710049,
+      dispatcherBlock: 24710049,
+      firstVaultBlock: 24710049,
     },
   },
 };
@@ -40,14 +44,14 @@ export const configure: Configurator<Variables> = (variables) => ({
   sources: [
     {
       name: 'Dispatcher',
-      block: variables.block,
+      block: variables.dispatcherBlock,
       address: variables.dispatcher,
       events: ['VaultProxyDeployed(indexed address,indexed address,address,indexed address,address,string)'],
     },
     {
       name: 'Asset',
       abi: 'ERC20Contract', // Inherited from global abis.
-      block: variables.block,
+      block: variables.firstVaultBlock,
       events: ['Transfer(indexed address,indexed address,uint256)'],
     },
   ],
