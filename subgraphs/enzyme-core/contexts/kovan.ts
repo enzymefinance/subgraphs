c7a8794d009a3ceb852b99991089bed69c7723b1
Ivan Herger <ivan.herger@gmail.com>
2021-08-26 10:30:36 +0200

    chore: prepare kovan and local-mainnet contexts

--- subgraphs/enzyme-core/contexts/kovan.ts
+++ subgraphs/enzyme-core/contexts/kovan.ts
@@ -7,7 +7,8 @@ export const kovanContext: Context<Variables> = {
   variables: {
     block: 24710049,
     dispatcherAddress: '0xC3DC853dD716bd5754f421ef94fdCbac3902ab32',
-    releaseA: {
+    wethTokenAddress: '0x0000000000000000000000000000000000000000',
+    release2: {
       fundDeployerAddress: '0xC5bADFDa459c329d20AA155a95dC43BA04c71228',
       vaultLibAddress: '0xe5cf8b7dd03e2cb3A9A5b1237bb8e99DbaC29704',
       comptrollerLibAddress: '0x55dbd3b92CEC623Ea5B37BBb9ED5Ec086887AA1A',
@@ -19,23 +20,6 @@ export const kovanContext: Context<Variables> = {
       // Prices
       aggregatedDerivativePriceFeedAddress: '0x1899F9e144A0D47cC1471e797C2b7930adf530b3',
       chainlinkPriceFeedAddress: '0x1dbf40Fc502A61a09c38F5D0f4D07f42AC507606',
-
-      // Derivative Price Feeds
-      aavePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      alphaHomoraV1PriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      chaiPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      compoundPriceFeedAddress: '0xd54CD71FD75A23FA8e4df44E4176e17Eb6f2B36D',
-      curvePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      idlePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      lidoStethPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      stakehoundEthPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      synthetixPriceFeedAddress: '0x4BA8f54EC8f11e69e95933Cc894cC726567072C0',
-      uniswapV2PoolPriceFeedAddress: '0xA311e4F6719e3eD91ce3f7f20294009AcF0BF0D3',
-      wdgldPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-
-      // Peripheral
-      fundActionsWrapperAddress: '0x7954a509058CB9a4F3169Df64D5332793092219E',
-      authUserExecutedSharesRequestorFactoryAddress: '0x763AAAAA45A9761fD21ab0cA71c76b5e075Bc064',
 
       // Fees
       managementFeeAddress: '0xda3400c7aBA11e683f4eb9b8f94B202C49fBdFaa',
@@ -53,26 +37,8 @@ export const kovanContext: Context<Variables> = {
       maxConcentrationAddress: '0xDF7A8AB72246bA6ea551bfFd7259dBD7ff4C6848',
       minMaxInvestmentAddress: '0x18b292A0A9E1092572d73D105e621f93f2b7f484',
       buySharesCallerWhitelistAddress: '0x2a0e8e532440a477d0C1520F674356eAbd9fB5Cc',
-
-      // Adapters
-      aaveAdapterAddress: '0x0000000000000000000000000000000000000000',
-      alphaHomoraV1AdapterAddress: '0x0000000000000000000000000000000000000000',
-      chaiAdapterAddress: '0x0000000000000000000000000000000000000000',
-      compoundAdapterAddress: '0xe759441dA6BCfba3a389384638d415eE80B2883B',
-      curveExchangeAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquidityAaveAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquiditySethAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquidityStethAdapterAddress: '0x0000000000000000000000000000000000000000',
-      idleAdapterAddress: '0x0000000000000000000000000000000000000000',
-      kyberAdapterAddress: '0x367C5084781bFebe3aE56d2387b70187F154BCd5',
-      paraSwapAdapterAddress: '0x0000000000000000000000000000000000000000',
-      paraSwapV4AdapterAddress: '0x0000000000000000000000000000000000000000',
-      synthetixAdapterAddress: '0x810809f334A3bf96437adC7540C0D74bdA653aD8',
-      trackedAssetsAdapterAddress: '0x311c8CfBFF5fBC79b0F3926B1C52A37dcd0257Af',
-      uniswapV2AdapterAddress: '0x3d3887758023a6a1DCAF999E5429055Fb43CAfaE',
-      zeroExV2AdapterAddress: '0x1dEbC247ecCB10dA06D42D6590E6FE19bc9a87FA',
     },
-    releaseB: {
+    release3: {
       fundDeployerAddress: '0x8375F0423C1DE0D8aCDCa79224F3d47fE8E6E197',
       vaultLibAddress: '0x7264721E8108e20c23592AaeCf923f1B84ce0A6a',
       comptrollerLibAddress: '0x9e8C31Df3d54Bbd1a9962BD8A3d69543e6b4fEAd',
@@ -84,23 +50,6 @@ export const kovanContext: Context<Variables> = {
       // Prices
       aggregatedDerivativePriceFeedAddress: '0x5304c3f2c2433e5e37732B46604eEB39725a4883',
       chainlinkPriceFeedAddress: '0x5A49D2a6420362bE3E396C59Fe9280c9f9588Ec3',
-
-      // Derivative Price Feeds
-      aavePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      alphaHomoraV1PriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      chaiPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      compoundPriceFeedAddress: '0x1a093b133B0f7E791b0F36eC5b5C50628f02d74F',
-      curvePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      idlePriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      lidoStethPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      stakehoundEthPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      synthetixPriceFeedAddress: '0x720056BF7476463428Ab0db0C75e49b83102B3be',
-      uniswapV2PoolPriceFeedAddress: '0xBC73c581BA1d2AA83e6575e85f2C2aE6284fA60F',
-      wdgldPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-
-      // Peripheral
-      fundActionsWrapperAddress: '0x6F00910dB9009c950c5a7c75AE10Aa529b18308A',
-      authUserExecutedSharesRequestorFactoryAddress: '0x829f1954a7682aCCad8c0869a3eF04125d93A12f',
 
       // Fees
       managementFeeAddress: '0x7E8E91FCf0Ad73e20AdEe711Eb9E21FE65B90E60',
@@ -118,24 +67,41 @@ export const kovanContext: Context<Variables> = {
       maxConcentrationAddress: '0xe640A756184FCDeFBE3228a2297dEb8cAF19AA61',
       minMaxInvestmentAddress: '0x367775E982408BC9326abAC10DF7D30480Cc02E1',
       buySharesCallerWhitelistAddress: '0x5EDDDCD4737AE331A730Ab25b764Dc3B9D3882fe',
+    },
+    release4: {
+      fundDeployerAddress: '0x0000000000000000000000000000000000000000',
+      vaultLibAddress: '0x0000000000000000000000000000000000000000',
+      comptrollerLibAddress: '0x0000000000000000000000000000000000000000',
+      valueInterpreterAddress: '0x0000000000000000000000000000000000000000',
+      integrationManagerAddress: '0x0000000000000000000000000000000000000000',
+      policyManagerAddress: '0x0000000000000000000000000000000000000000',
+      feeManagerAddress: '0x0000000000000000000000000000000000000000',
+      globalConfigLibAddress: '0x0000000000000000000000000000000000000000',
 
-      // Adapters
-      aaveAdapterAddress: '0x0000000000000000000000000000000000000000',
-      alphaHomoraV1AdapterAddress: '0x0000000000000000000000000000000000000000',
-      chaiAdapterAddress: '0x0000000000000000000000000000000000000000',
-      compoundAdapterAddress: '0xbfdcfD59Bb50d61CcB2a8dC068fA6d1c5071a3e8',
-      curveExchangeAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquidityAaveAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquiditySethAdapterAddress: '0x0000000000000000000000000000000000000000',
-      curveLiquidityStethAdapterAddress: '0x0000000000000000000000000000000000000000',
-      idleAdapterAddress: '0x0000000000000000000000000000000000000000',
-      kyberAdapterAddress: '0xd0dc89F8bff640DC399D9d56f451d7be182f792B',
-      paraSwapAdapterAddress: '0x0000000000000000000000000000000000000000',
-      paraSwapV4AdapterAddress: '0x0000000000000000000000000000000000000000',
-      synthetixAdapterAddress: '0xD9ba34570313887275Bf6d4e6e033F1b138fb97f',
-      trackedAssetsAdapterAddress: '0x837d050F4a9eed50E32dAc9571Cf22F064514891',
-      uniswapV2AdapterAddress: '0x667807bbC182D61bA6C0989dA4A8Da65f0320C7D',
-      zeroExV2AdapterAddress: '0x780e7B642BB60d1037a6ca373CfF0aEA5269Dfa9',
+      // External Positions
+      externalPositionFactoryAddress: '0x0000000000000000000000000000000000000000',
+      externalPositionManagerAddress: '0x0000000000000000000000000000000000000000',
+
+      // Gas Relayer
+      gasRelayPaymasterFactoryAddress: '0x0000000000000000000000000000000000000000',
+
+      // Protocol Feed
+      protocolFeeReserveLibAddress: '0x0000000000000000000000000000000000000000',
+      protocolFeeTrackerAddress: '0x0000000000000000000000000000000000000000',
+
+      // Fees
+      managementFeeAddress: '0x0000000000000000000000000000000000000000',
+      performanceFeeAddress: '0x0000000000000000000000000000000000000000',
+      entranceRateBurnFeeAddress: '0x0000000000000000000000000000000000000000',
+      entranceRateDirectFeeAddress: '0x0000000000000000000000000000000000000000',
+      exitRateBurnFeeAddress: '0x0000000000000000000000000000000000000000',
+      exitRateDirectFeeAddress: '0x0000000000000000000000000000000000000000',
+
+      // Policies
+      allowedAdapterIncomingAssetsPolicyAddress: '0x0000000000000000000000000000000000000000',
+      allowedDepositRecipientsPolicyAddress: '0x0000000000000000000000000000000000000000',
+      guaranteedRedemptionPolicyAddress: '0x0000000000000000000000000000000000000000',
+      minMaxInvestmentPolicyAddress: '0x0000000000000000000000000000000000000000',
     },
   },
 };
--- subgraphs/enzyme-core/contexts/local-mainnet.ts
+++ subgraphs/enzyme-core/contexts/local-mainnet.ts
@@ -8,7 +8,8 @@ export const localMainnetContext: Context<Variables> = {
   variables: {
     block: 11636493,
     dispatcherAddress: '0xC3DC853dD716bd5754f421ef94fdCbac3902ab32',
-    releaseA: {
+    wethTokenAddress: '0x0000000000000000000000000000000000000000',
+    release2: {
       fundDeployerAddress: '0x9134C9975244b46692Ad9A7Da36DBa8734Ec6DA3',
       vaultLibAddress: '0xAC3fe07F51C51153E181bE892e4e37326EEA13Da',
       comptrollerLibAddress: '0x94F262802806BE3646612D0705802710dD5B58dF',
@@ -20,23 +21,6 @@ export const localMainnetContext: Context<Variables> = {
       // Prices
       aggregatedDerivativePriceFeedAddress: '0xCFb6F4C08856986d13839B1907b5c645EE95388F',
       chainlinkPriceFeedAddress: '0xfB3A9655F5feA18caC92021E83550F829ae6F7F7',
-
-      // Derivative Price Feeds
-      aavePriceFeedAddress: '0x3996D53013f562FD6D5B777Ab0Ee681802698e0D',
-      alphaHomoraV1PriceFeedAddress: '0x5f2E2E7FfbcAeE69d570D531A00228322632231E',
-      chaiPriceFeedAddress: '0x8bA6f2D9C81Db32665df6a926227dD00cacA7d9B',
-      compoundPriceFeedAddress: '0xC80bD25cd46e49277CcB56E751704A6316Af30aD',
-      curvePriceFeedAddress: '0x884D909EA86EC61128a4cFE1B455d5B440cdE9C8',
-      idlePriceFeedAddress: '0x08eCd9e1378DC0262df826C07D3D64c728a46829',
-      lidoStethPriceFeedAddress: '0x4F3Da46BF999C6b3005C1465672009fBF99e7b9f',
-      stakehoundEthPriceFeedAddress: '0xe2b19fa2d662f4eea51d394b71ce7281214840c5',
-      synthetixPriceFeedAddress: '0x2180341339010C237FF8e3dd1fF24063FB18C4D0',
-      uniswapV2PoolPriceFeedAddress: '0x9177A3354EE50bffBcC42C4c6baC27ed63979097',
-      wdgldPriceFeedAddress: '0x45919dcDECcA3C7a0D29f959E6b78a605a17A3A2',
-
-      // Peripheral
-      fundActionsWrapperAddress: '0x38c9f2870003d47f704e317C10f93Ca1ddAE67C1',
-      authUserExecutedSharesRequestorFactoryAddress: '0x402dC740e4C266C640C4477b5649c75A86AAF780',
 
       // Fees
       managementFeeAddress: '0x889f2FCB6c12d836cB8f7567A1bdfa512FE9f647',
@@ -54,26 +38,8 @@ export const localMainnetContext: Context<Variables> = {
       maxConcentrationAddress: '0x6b9a7604faB14721B51A89Cd1CcEF639Da8664eE',
       minMaxInvestmentAddress: '0xDCCae078656f20B0851Dd35683315be56AEEe8f3',
       buySharesCallerWhitelistAddress: '0x21fc97450c65dAfe14255A06Cf9c5C88483bA309',
-
-      // Adapters
-      aaveAdapterAddress: '0x14c6b99AfFC61e9b0753146F3437A223d0c58279',
-      alphaHomoraV1AdapterAddress: '0xed6Bdfa2725da6687bC89Bcb0BDB1e5dEAe15838',
-      chaiAdapterAddress: '0xB3646e9B6dF463a2dC36de67FFD28cbAb3f8A5cC',
-      compoundAdapterAddress: '0xD050DC9E75f24Ae653d282D0cFb772871729e710',
-      curveExchangeAdapterAddress: '0x402A81aD2972a017B4564453E69afaE2b006A7E2',
-      curveLiquidityAaveAdapterAddress: '0xF6F7cc9464Cb15e0A5B116D738dCa88434bbA00A',
-      curveLiquiditySethAdapterAddress: '0xbD81a0eF03F4eD89F73a30eb33137787BF92e177',
-      curveLiquidityStethAdapterAddress: '0x3Bb38E67033e24E604e53964A99D914f8B216521',
-      idleAdapterAddress: '0x2f7918d9a98c199173fb8ca989b408b4fb1ea441',
-      kyberAdapterAddress: '0x963E9C15a26d74085ABfC8B48280b650426F998e',
-      paraSwapAdapterAddress: '0x56a1892b2276bbb9968d1b5aa0000a71bf0fa7b8',
-      paraSwapV4AdapterAddress: '0x1c1b94812faf3de496a2634b36c1fbc6e5c222e7',
-      synthetixAdapterAddress: '0xBF3411973F09c32E8c4e67d1345DdDC5F9c7535a',
-      trackedAssetsAdapterAddress: '0xde4A42052db0FB0E8935C9F3015b8cd56A3dDF43',
-      uniswapV2AdapterAddress: '0x972318a0f4935c3153a2AA4c81274DfF621B360b',
-      zeroExV2AdapterAddress: '0x4205073e7AD2F9896F827DBCe496dd2306DB602e',
     },
-    releaseB: {
+    release3: {
       fundDeployerAddress: '0x7e6d3b1161DF9c9c7527F68d651B297d2Fdb820B',
       vaultLibAddress: '0x1b3694907Ed7459c7B0116e7c6a4B7788288577F',
       comptrollerLibAddress: '0x1A7CBEDf13E25818dC377d2B1277E03e5d496300',
@@ -85,23 +51,6 @@ export const localMainnetContext: Context<Variables> = {
       // Prices
       aggregatedDerivativePriceFeedAddress: '0x2E45f9B3fd5871cCaf4eB415dFCcbDD126F57C4f',
       chainlinkPriceFeedAddress: '0x1fad8faf11E027f8630F394599830dBeb97004EE',
-
-      // Derivative Price Feeds
-      aavePriceFeedAddress: '0x4e49a272dC42E26C8772de366aaa93d1AC816794',
-      alphaHomoraV1PriceFeedAddress: '0x1a8b4f6D469Ee775d5982C4d4AAb46677c2C92d4',
-      chaiPriceFeedAddress: '0x0000000000000000000000000000000000000000',
-      compoundPriceFeedAddress: '0x6d7F71Cc3109D4132Ad6124D84e72E353b979880',
-      curvePriceFeedAddress: '0xc106f1B01017c854a9cd2D88Db733408236DD809',
-      idlePriceFeedAddress: '0x13c2263e534BD27149d96b8Cb9961ea1beB560Ef',
-      lidoStethPriceFeedAddress: '0x11D8d414724281BD702838BA16C8F15F7C473e9a',
-      stakehoundEthPriceFeedAddress: '0xd3C515EAD7Bcc7451cd920e066C4a1849B827dfD',
-      synthetixPriceFeedAddress: '0x4158F02ac2fF0f0E7959A768D06619B685aBF0C8',
-      uniswapV2PoolPriceFeedAddress: '0x37FFB1842eF012d83A6FFdc92A6353044Ae0e3e8',
-      wdgldPriceFeedAddress: '0x76b548553d4E729c81e8ae8Ab826795bd88BA9E7',
-
-      // Peripheral
-      fundActionsWrapperAddress: '0xbc9a63cad5a825bD5854Abca4f3D42d6aCF9Bffa',
-      authUserExecutedSharesRequestorFactoryAddress: '0x0000000000000000000000000000000000000000',
 
       // Fees
       managementFeeAddress: '0x45f1b268cc6412b454dae20f8971fc4a36af0d29',
@@ -119,24 +68,41 @@ export const localMainnetContext: Context<Variables> = {
       maxConcentrationAddress: '0xdee63b253432a443a09bf8461f20256d184319de',
       minMaxInvestmentAddress: '0x016a7287f0fdbdce5f903334f574b2238be3fa25',
       buySharesCallerWhitelistAddress: '0xbc91dcc90ccdd0b51e3c623528cb886358c60105',
+    },
+    release4: {
+      fundDeployerAddress: '0x0000000000000000000000000000000000000000',
+      vaultLibAddress: '0x0000000000000000000000000000000000000000',
+      comptrollerLibAddress: '0x0000000000000000000000000000000000000000',
+      valueInterpreterAddress: '0x0000000000000000000000000000000000000000',
+      integrationManagerAddress: '0x0000000000000000000000000000000000000000',
+      policyManagerAddress: '0x0000000000000000000000000000000000000000',
+      feeManagerAddress: '0x0000000000000000000000000000000000000000',
+      globalConfigLibAddress: '0x0000000000000000000000000000000000000000',
 
-      // Adapters
-      aaveAdapterAddress: '0x0Bad0886c5976072549687D5FfCD26803C5bA167',
-      alphaHomoraV1AdapterAddress: '0x63765Ae5Df9bB8AF9D8bc976E595b79907CF341c',
-      chaiAdapterAddress: '0x0000000000000000000000000000000000000000',
-      compoundAdapterAddress: '0xb7bfd3582582A82Ef2A2f0Df0adf705f35A07D4B',
-      curveExchangeAdapterAddress: '0x1990D8Bc382D38fA21EE3B570e9d1E6272152165',
-      curveLiquidityAaveAdapterAddress: '0x78bA80C8517A4f3146FD76A3642Fa57B77F331eb',
-      curveLiquiditySethAdapterAddress: '0xeE91E3bB4752F358DDAaC6ab825536698b239546',
-      curveLiquidityStethAdapterAddress: '0x6367bb496390dDf0ef508Db2992F53DBf376b1f4',
-      idleAdapterAddress: '0x649b6cc835c8fe3d3b94b6a9c155f791bf500dfe',
-      kyberAdapterAddress: '0x4ff16EfF3b6D2175A513EF4c5b95f5f4D2F05179',
-      paraSwapAdapterAddress: '0x0000000000000000000000000000000000000000',
-      paraSwapV4AdapterAddress: '0x8eA6Ca02274E1b05b70c11058213ADC667258C3D',
-      synthetixAdapterAddress: '0x07698c3fa26a62ec6320872bf2950ba160a2dd7c',
-      trackedAssetsAdapterAddress: '0x2e8b6455cb06463a523d3318f2cdc6a55ed45118',
-      uniswapV2AdapterAddress: '0x8481150a0f36c98EE859e6C7B46d61FDD836768f',
-      zeroExV2AdapterAddress: '0x9b6cD2195d3c275875cB469A59dd437995712550',
+      // External Positions
+      externalPositionFactoryAddress: '0x0000000000000000000000000000000000000000',
+      externalPositionManagerAddress: '0x0000000000000000000000000000000000000000',
+
+      // Gas Relayer
+      gasRelayPaymasterFactoryAddress: '0x0000000000000000000000000000000000000000',
+
+      // Protocol Feed
+      protocolFeeReserveLibAddress: '0x0000000000000000000000000000000000000000',
+      protocolFeeTrackerAddress: '0x0000000000000000000000000000000000000000',
+
+      // Fees
+      managementFeeAddress: '0x0000000000000000000000000000000000000000',
+      performanceFeeAddress: '0x0000000000000000000000000000000000000000',
+      entranceRateBurnFeeAddress: '0x0000000000000000000000000000000000000000',
+      entranceRateDirectFeeAddress: '0x0000000000000000000000000000000000000000',
+      exitRateBurnFeeAddress: '0x0000000000000000000000000000000000000000',
+      exitRateDirectFeeAddress: '0x0000000000000000000000000000000000000000',
+
+      // Policies
+      allowedAdapterIncomingAssetsPolicyAddress: '0x0000000000000000000000000000000000000000',
+      allowedDepositRecipientsPolicyAddress: '0x0000000000000000000000000000000000000000',
+      guaranteedRedemptionPolicyAddress: '0x0000000000000000000000000000000000000000',
+      minMaxInvestmentPolicyAddress: '0x0000000000000000000000000000000000000000',
     },
   },
 };
