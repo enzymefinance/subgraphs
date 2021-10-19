3bbaac03276c0919ccae3b66e879f9c7fc9d9767
Sebastian Siemssen <fubhy@fubhy.com>
2021-10-19 18:38:50 +0200

    feat: link details also to change records

--- subgraphs/asset-universe/entities/Registration.ts
+++ subgraphs/asset-universe/entities/Registration.ts
@@ -1,12 +1,11 @@
-import { arrayDiff, arrayUnique } from '@enzymefinance/subgraph-utils';
-import { Address, ethereum } from '@graphprotocol/graph-ts';
+import { arrayUnique } from '@enzymefinance/subgraph-utils';
+import { Address, Entity, ethereum } from '@graphprotocol/graph-ts';
 import {
   Asset,
   DerivativeRegistrationDetails,
   PrimitiveRegistrationDetails,
   Registration,
-  RegistrationAdded,
-  RegistrationRemoved,
+  RegistrationChange,
   Version,
 } from '../generated/schema';
 import { getRegistrationChangeCounter, getRegistrationCounter } from './Counter';
@@ -32,21 +31,21 @@ export function getOrCreateRegistration(version: Version, asset: Asset): Registr
   return registration;
 }
 
-function registrationDetailsId(registration: Registration, type: string): string {
-  return registration.id + '/' + type;
+function registrationDetailsId(version: Version, asset: Asset, event: ethereum.Event): string {
+  return version.id + '/' + asset.id + '/' + event.transaction.hash.toHex() + '/' + event.logIndex.toString();
 }
 
 export function getOrCreateDerivativeRegistrationDetails(
-  registration: Registration,
+  version: Version,
+  asset: Asset,
   feed: Address,
+  event: ethereum.Event,
 ): DerivativeRegistrationDetails {
-  let id = registrationDetailsId(registration, 'DERIVATIVE');
-
-  let details = DerivativeRegistrationDetails.load(registration.id);
+  let id = registrationDetailsId(version, asset, event);
+  let details = DerivativeRegistrationDetails.load(id);
 
   if (details == null) {
     details = new DerivativeRegistrationDetails(id);
-    details.registration = registration.id;
     details.feed = feed;
     details.save();
   }
@@ -55,21 +54,48 @@ export function getOrCreateDerivativeRegistrationDetails(
 }
 
 export function getOrCreatePrimitiveRegistrationDetails(
-  registration: Registration,
+  version: Version,
+  asset: Asset,
   aggregator: Address,
+  event: ethereum.Event,
 ): PrimitiveRegistrationDetails {
-  let id = registrationDetailsId(registration, 'PRIMITIVE');
-
-  let details = PrimitiveRegistrationDetails.load(registration.id);
+  let id = registrationDetailsId(version, asset, event);
+  let details = PrimitiveRegistrationDetails.load(id);
 
   if (details == null) {
     details = new PrimitiveRegistrationDetails(id);
-    details.registration = registration.id;
     details.aggregator = aggregator;
     details.save();
   }
 
   return details;
+}
+
+function registrationChangeId(registration: Registration, change: string, event: ethereum.Event): string {
+  return registration.id + '/' + change + '/' + event.transaction.hash.toHex() + '/' + event.logIndex.toString();
+}
+
+export function recordRegistrationChange(
+  version: Version,
+  asset: Asset,
+  registration: Registration,
+  details: string,
+  change: string,
+  event: ethereum.Event,
+): void {
+  let id = registrationChangeId(registration, change, event);
+  let record = new RegistrationChange(id);
+  record.registration = registration.id;
+  record.details = details;
+  record.type = 'DERIVATIVE';
+  record.change = 'ADDED';
+  record.version = version.id;
+  record.asset = asset.id;
+  record.timestamp = event.block.timestamp.toI32();
+  record.block = event.block.number;
+  record.transaction = event.transaction.hash;
+  record.counter = getRegistrationChangeCounter();
+  record.save();
 }
 
 export function createDerivativeRegistration(
@@ -79,8 +105,7 @@ export function createDerivativeRegistration(
   event: ethereum.Event,
 ): void {
   let registration = getOrCreateRegistration(version, asset);
-  let details = getOrCreateDerivativeRegistrationDetails(registration, feed);
-
+  let details = getOrCreateDerivativeRegistrationDetails(version, asset, feed, event);
   registration.active = true;
   registration.details = details.id;
   registration.save();
@@ -88,39 +113,17 @@ export function createDerivativeRegistration(
   asset.registrations = arrayUnique<string>(asset.registrations.concat([registration.id]));
   asset.save();
 
-  let change = new RegistrationAdded(registration.id + '/ADDED' + '/' + event.block.timestamp.toString());
-  change.registration = registration.id;
-  change.type = 'DERIVATIVE';
-  change.change = 'ADDED';
-  change.version = version.id;
-  change.asset = asset.id;
-  change.timestamp = event.block.timestamp.toI32();
-  change.block = event.block.number;
-  change.transaction = event.transaction.hash;
-  change.counter = getRegistrationChangeCounter();
-  change.save();
+  // Record the change with the new details.
+  recordRegistrationChange(version, asset, registration, details.id, 'ADDED', event);
 }
 
 export function removeDerivativeRegistration(version: Version, asset: Asset, event: ethereum.Event): void {
   let registration = getOrCreateRegistration(version, asset);
-
   registration.active = false;
   registration.save();
 
-  asset.registrations = arrayDiff<string>(asset.registrations, [registration.id]);
-  asset.save();
-
-  let id = registrationId(version, asset);
-  let change = new RegistrationRemoved(id + '/REMOVED');
-  change.type = 'DERIVATIVE';
-  change.change = 'REMOVED';
-  change.version = version.id;
-  change.asset = asset.id;
-  change.timestamp = event.block.timestamp.toI32();
-  change.block = event.block.number;
-  change.transaction = event.transaction.hash;
-  change.counter = getRegistrationChangeCounter();
-  change.save();
+  // Record the change with the last known details.
+  recordRegistrationChange(version, asset, registration, registration.details, 'REMOVED', event);
 }
 
 export function createPrimitiveRegistration(
@@ -130,8 +133,7 @@ export function createPrimitiveRegistration(
   event: ethereum.Event,
 ): void {
   let registration = getOrCreateRegistration(version, asset);
-  let details = getOrCreateDerivativeRegistrationDetails(registration, aggregator);
-
+  let details = getOrCreatePrimitiveRegistrationDetails(version, asset, aggregator, event);
   registration.active = true;
   registration.details = details.id;
   registration.save();
@@ -139,17 +141,8 @@ export function createPrimitiveRegistration(
   asset.registrations = arrayUnique<string>(asset.registrations.concat([registration.id]));
   asset.save();
 
-  let change = new RegistrationAdded(registration.id + '/ADDED' + '/' + event.block.timestamp.toString());
-  change.registration = registration.id;
-  change.type = 'PRIMITIVE';
-  change.change = 'ADDED';
-  change.version = version.id;
-  change.asset = asset.id;
-  change.timestamp = event.block.timestamp.toI32();
-  change.block = event.block.number;
-  change.transaction = event.transaction.hash;
-  change.counter = getRegistrationChangeCounter();
-  change.save();
+  // Record the change with the new details.
+  recordRegistrationChange(version, asset, registration, details.id, 'ADDED', event);
 }
 
 export function removePrimitiveRegistration(version: Version, asset: Asset, event: ethereum.Event): void {
@@ -157,17 +150,6 @@ export function removePrimitiveRegistration(version: Version, asset: Asset, even
   registration.active = false;
   registration.save();
 
-  asset.registrations = arrayDiff<string>(asset.registrations, [registration.id]);
-  asset.save();
-
-  let change = new RegistrationRemoved(registration.id + '/REMOVED' + '/' + event.block.timestamp.toString());
-  change.type = 'PRIMITIVE';
-  change.change = 'REMOVED';
-  change.version = version.id;
-  change.asset = asset.id;
-  change.timestamp = event.block.timestamp.toI32();
-  change.block = event.block.number;
-  change.transaction = event.transaction.hash;
-  change.counter = getRegistrationChangeCounter();
-  change.save();
+  // Record the change with the last known details.
+  recordRegistrationChange(version, asset, registration, registration.details, 'REMOVED', event);
 }
