51edd73f5b57f277c7cf4c017ef2d5323d656e8a
Sebastian Siemssen <fubhy@fubhy.com>
2022-07-01 16:39:20 +0200

    feat: add token burn stats from old token contract

--- subgraphs/mln-token/mappings/Token.ts
+++ subgraphs/mln-token/mappings/Token.ts
@@ -1,22 +1,71 @@
 import { ZERO_BD, toBigDecimal, ZERO_ADDRESS } from '@enzymefinance/subgraph-utils';
-import { Address, Bytes } from '@graphprotocol/graph-ts';
+import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts';
 import { Transfer as TransferEvent } from '../generated/contracts/TokenEvents';
-import { Account, Transfer } from '../generated/schema';
+import { Account, Balance, Transfer } from '../generated/schema';
 
-export function getOrCreateAccount(address: Address): Account {
+// When the new token contract was deployed, the migration contract got minted a tranch of 932613000000000000000000 tokens. It is
+// unclear how this value came to be but as a matter of fact this is now the total sum of all tokens that can ever be migrated over
+// from the old contract. Hence, we can consider the difference of the total supply of the old token and this value as the amount
+// of tokens that were burned at this point.
+//
+// Hence, 1250000000000000000000000 - 932613000000000000000000 = 317387000000000000000000 ...
+//
+// # Some other notes:
+//
+// ## Old token
+//
+// Address: https://etherscan.io/address/0xbeb9ef514a379b997e0798fdcc901ee474b6d9a1
+// Deployment: https://etherscan.io/tx/0x024ee6228b799a5ce93ccc94a8e3b657e960ae53222fd45e250b2984905f3e2
+// Burn: https://etherscan.io/tx/0x7dbf771925bec2489f398a98f3156da4d0b19cdf55e6da57cb8701617f3fd45d
+//
+// Supply: 1250000000000000000000000
+// Burned: 248011981318720000000000 at block 7130390
+//
+// ## New token
+//
+// Address: https://etherscan.io/address/0xec67005c4E498Ec7f55E092bd1d35cbC47C91892
+// Deployment: https://etherscan.io/tx/0xaffb53cd29ea4ea81b9131d43cc2a02ae5d6f0268d92336daa5c672084f53ec5
+// Minting: https://etherscan.io/tx/0xd8d5a28a698a5748f4785df2627a777a6cd590e956cb98d0d2b4fcf084bc1a85
+//
+// Minted: 932613000000000000000000 at block 7130390
+//
+// ## Token migration contract
+//
+// Address: https://etherscan.io/address/0x3c75d37b579e0e4896f02c0122baa4de05383a6a
+// Deployment: https://etherscan.io/tx/0x5ab5e0d8e7a8b22b69346bedc71086ac6d32fee0b78560d9dba8859ec8db50f2
+
+export function getOrCreateAccount(address: Address, event: ethereum.Event): Account {
   let account = Account.load(address);
 
   if (account == null) {
     account = new Account(address);
     account.balance = ZERO_BD;
     account.save();
+
+    if (address.equals(ZERO_ADDRESS)) {
+      // NOTE: This is the balance of the zero address at block 15056726 which we use as a start block to track
+      // burn events on the old token contract.
+      account.balance = toBigDecimal(BigInt.fromString('317387000000000000000000'), 18);
+      account.save();
+
+      let balance = new Balance(account.id.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.block.number))));
+      balance.block = event.block.number;
+      balance.account = account.id;
+      balance.timestamp = event.block.timestamp;
+      balance.balance = account.balance;
+      balance.save();
+    }
   }
 
   return account;
 }
 
+function getTransferType(from: Address, to: Address): string {
+  return from.equals(ZERO_ADDRESS) ? 'MINT' : to.equals(ZERO_ADDRESS) ? 'BURN' : 'TRANSFER';
+}
+
 export function handleTransfer(event: TransferEvent): void {
-  if (event.params.to.equals(ZERO_ADDRESS) && event.params.from.equals(ZERO_ADDRESS)) {
+  if (event.params.from.equals(event.params.to)) {
     return;
   }
 
@@ -27,28 +76,33 @@ export function handleTransfer(event: TransferEvent): void {
   transfer.from = event.params.from;
   transfer.to = event.params.to;
   transfer.amount = toBigDecimal(event.params.value, 18);
+  transfer.type = getTransferType(event.params.from, event.params.to);
+  transfer.save();
 
-  let to = getOrCreateAccount(event.params.to);
-  let from = getOrCreateAccount(event.params.from);
+  let toAccount = getOrCreateAccount(event.params.to, event);
+  let fromAccount = getOrCreateAccount(event.params.from, event);
 
-  if (event.params.from.equals(ZERO_ADDRESS)) {
-    transfer.type = 'MINT';
-    transfer.save();
+  toAccount.balance = toAccount.balance.plus(transfer.amount);
+  toAccount.save();
 
-    // When minting, the tokens are not actually transferred from the zero address, hence we don't want to reduce its balance in this case.
-    to.balance = to.balance.plus(transfer.amount);
-    to.save();
-  } else {
-    transfer.type = event.params.to.equals(ZERO_ADDRESS) ? 'BURN' : 'TRANSFER';
-    transfer.save();
+  let toBalance = new Balance(toAccount.id.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.block.number))));
+  toBalance.block = event.block.number;
+  toBalance.account = toAccount.id;
+  toBalance.timestamp = event.block.timestamp;
+  toBalance.balance = toAccount.balance;
+  toBalance.save();
 
-    // We want to use the account at the zero address to record the total amount of tokens burned. Hence we do not make a difference here.
-    to.balance = to.balance.plus(transfer.amount);
-    to.save();
+  // Do not deduct the balance from the zero address in case of a token mint so we can use this
+  // balance to track the total amount of burned tokens.
+  if (transfer.type != 'MINT') {
+    fromAccount.balance = fromAccount.balance.minus(transfer.amount);
+    fromAccount.save();
 
-    from.balance = from.balance.minus(transfer.amount);
-    from.save();
+    let fromBalance = new Balance(fromAccount.id.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.block.number))));
+    fromBalance.block = event.block.number;
+    fromBalance.account = fromAccount.id;
+    fromBalance.timestamp = event.block.timestamp;
+    fromBalance.balance = fromAccount.balance;
+    fromBalance.save();
   }
-
-  // TODO: Record balance history.
 }
