7f3992195c660c9c1d8846392cdc2386c7f1c013
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-02-16 16:58:22 +0100

    feat: add util functions for id and type cases

---
+++ subgraphs/enzyme-core/utils/notionalV2AssetByCurrencyId.ts
@@ -0,0 +1,23 @@
+import { wethTokenAddress, daiAddress, usdcAddress, wbtcAddress } from '../generated/addresses';
+import { Asset } from '../generated/schema';
+import { ensureAsset } from '../entities/Asset';
+
+export function notionalV2AssetByCurrencyId(type: number): Asset | null {
+  if (type == 1) {
+    return ensureAsset(wethTokenAddress);
+  }
+
+  if (type == 2) {
+    return ensureAsset(daiAddress);
+  }
+
+  if (type == 3) {
+    return ensureAsset(usdcAddress);
+  }
+
+  if (type == 4) {
+    return ensureAsset(wbtcAddress);
+  }
+
+  return null;
+}
---
+++ subgraphs/enzyme-core/utils/notionalV2MarketIndexType.ts
@@ -0,0 +1,31 @@
+export function notionalV2MarketIndexType(type: string): string {
+  if (type == '1') {
+    return '3 month';
+  }
+
+  if (type == '2') {
+    return '6 month';
+  }
+
+  if (type == '3') {
+    return '1 year';
+  }
+
+  if (type == '4') {
+    return '2 year';
+  }
+
+  if (type == '5') {
+    return '5 year';
+  }
+
+  if (type == '6') {
+    return '10 year';
+  }
+
+  if (type == '7') {
+    return '20 year';
+  }
+
+  return 'Unknown';
+}
