afc4a225241a4c6bb04025c323897049df092af6
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-02-19 16:55:03 +0100

    feat: add track notion position for lend and debt assets

--- subgraphs/enzyme-core/entities/NotionalV2Position.ts
+++ subgraphs/enzyme-core/entities/NotionalV2Position.ts
@@ -1,4 +1,4 @@
-import { logCritical, uniqueEventId } from '@enzymefinance/subgraph-utils';
+import { logCritical, toBigDecimal, uniqueEventId } from '@enzymefinance/subgraph-utils';
 import { Address, ethereum } from '@graphprotocol/graph-ts';
 import {
   AssetAmount,
@@ -8,8 +8,11 @@ import {
   Vault,
   Asset,
 } from '../generated/schema';
+import { ProtocolSdk } from '../generated/contracts/ProtocolSdk';
 import { useVault } from './Vault';
 import { getActivityCounter } from './Counter';
+import { ensureAsset } from './Asset';
+import { createAssetAmount } from './AssetAmount';
 
 export function useNotionalV2Position(id: string): NotionalV2Position {
   let nv2p = NotionalV2Position.load(id);
@@ -29,6 +32,8 @@ export function createNotionalV2Position(
   notionalV2Position.vault = useVault(vaultAddress.toHex()).id;
   notionalV2Position.active = true;
   notionalV2Position.type = type.id;
+  notionalV2Position.lendAssets = null;
+  notionalV2Position.borrowedAssets = null;
   notionalV2Position.save();
 
   return notionalV2Position;
@@ -50,7 +55,9 @@ export function createNotionalV2PositionChange(
   change.externalPosition = notionalV2PositionAddress.toHex();
   change.notionalV2PositionChangeType = changeType;
   change.incomingAsset = incomingAsset != null ? incomingAsset.id : null;
-  change.outgoingAssetAmount = outgoingAssetAmount != null ? outgoingAssetAmount.id : null;
+  change.incomingAssetAmount = incomingAssetAmount != null ? incomingAssetAmount : null;
+  change.outgoingAsset = outgoingAsset != null ? outgoingAsset.id : null;
+  change.outgoingAssetAmount = outgoingAssetAmount != null ? outgoingAssetAmount : null;
   change.fCashAmount = fCashAmount != null ? fCashAmount : null;
   change.maturity = maturity != null ? maturity : null;
   change.vault = vault.id;
@@ -64,4 +71,50 @@ export function createNotionalV2PositionChange(
   vault.save();
 
   return change;
+}
+
+export function trackNotionalV2Position(id: string, denominationAsset: Asset, event: ethereum.Event): void {
+  let nv2pContract = ProtocolSdk.bind(Address.fromString(id));
+
+  let nv2p = useNotionalV2Position(id);
+
+  let lendAssets = nv2pContract.getManagedAssets();
+
+  if (lendAssets.value0.length === 0) {
+    nv2p.lendAssets = null;
+  } else {
+    let addresses = lendAssets.value0;
+    let amounts = lendAssets.value1;
+    let assetAmounts: AssetAmount[] = new Array<AssetAmount>();
+
+    for (let i = 0; i < addresses.length; i++) {
+      let asset = ensureAsset(addresses[i]);
+      let amount = toBigDecimal(amounts[i], asset.decimals);
+      let assetAmount = createAssetAmount(asset, amount, denominationAsset, 'nv2p', event);
+      assetAmounts = assetAmounts.concat([assetAmount]);
+    }
+
+    nv2p.lendAssets = assetAmounts;
+  }
+
+  let borrowedAssets = nv2pContract.getDebtAssets();
+
+  if (borrowedAssets.value0.length === 0) {
+    nv2p.borrowedAssets = null;
+  } else {
+    let addresses = borrowedAssets.value0;
+    let amounts = borrowedAssets.value1;
+    let assetAmounts: AssetAmount[] = new Array<AssetAmount>();
+
+    for (let i = 0; i < addresses.length; i++) {
+      let asset = ensureAsset(addresses[i]);
+      let amount = toBigDecimal(amounts[i], asset.decimals);
+      let assetAmount = createAssetAmount(asset, amount, denominationAsset, 'nv2p', event);
+      assetAmounts = assetAmounts.concat([assetAmount]);
+    }
+
+    nv2p.borrowedAssets = assetAmounts;
+  }
+
+  nv2p.save();
 }
