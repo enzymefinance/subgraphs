1b0205415ce8c62d57c81098c06db0ea6c21b674
Ivan Herger <36133712+iherger@users.noreply.github.com>
2023-08-01 09:05:43 +0200

    feat: enable kiln withdrawal (#67)
    Co-authored-by: Sebastian Lorenz <fubhy@fubhy.com>

--- subgraphs/enzyme-core/mappings/v4/KilnStaking.ts
+++ subgraphs/enzyme-core/mappings/v4/KilnStaking.ts
@@ -1,9 +1,19 @@
-import { arrayUnique } from '@enzymefinance/subgraph-utils';
-import { useKilnStakingPosition } from '../../entities/KilnStakingPosition';
-import { Deposit } from '../../generated/contracts/KilnStaking4Events';
+import { ensureKilnStakingPositionValidator, useKilnStakingPosition } from '../../entities/KilnStakingPosition';
+import { Deposit, ExitRequest } from '../../generated/contracts/KilnStaking4Events';
 
 export function handleDeposit(event: Deposit): void {
   let kilnStakingPosition = useKilnStakingPosition(event.params.caller.toHex());
-  kilnStakingPosition.publicKeys = arrayUnique(kilnStakingPosition.publicKeys.concat([event.params.publicKey]));
-  kilnStakingPosition.save();
+
+  let validator = ensureKilnStakingPositionValidator(event.params.publicKey, kilnStakingPosition);
+  validator.createdAt = event.block.timestamp.toI32();
+  validator.save();
+}
+
+export function handleExitRequest(event: ExitRequest): void {
+  let kilnStakingPosition = useKilnStakingPosition(event.params.caller.toHex());
+
+  let validator = ensureKilnStakingPositionValidator(event.params.pubkey, kilnStakingPosition);
+  validator.unstakeSignalled = true;
+  validator.unstakeSignalledAt = event.block.timestamp.toI32();
+  validator.save();
 }
