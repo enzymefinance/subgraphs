a9d2af323a7aad5a4ea90c1a7ecbce126478bb79
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-11-23 16:03:57 +0100

    fix: decode asset configs on update settings

--- subgraphs/enzyme-core/mappings/v4/NoDepegOnRedeemSharesForSpecificAssetsPolicy.ts
+++ subgraphs/enzyme-core/mappings/v4/NoDepegOnRedeemSharesForSpecificAssetsPolicy.ts
@@ -1,9 +1,13 @@
+import { AssetConfig } from '../../generated/schema';
 import { ensureNoDepegOnRedeemSharesForSpecificAssetsPolicy } from '../../entities/NoDepegOnRedeemSharesForSpecificAssetsPolicy';
 import { FundSettingsUpdated } from '../../generated/contracts/NoDepegOnRedeemSharesForSpecificAssetsPolicy4Events';
+import { ensureAssetConfig } from '../../entities/AssetConfig';
 
-export function handleFundSettingsSet(event: FundSettingsUpdated): void {
+
+
+export function handleFundSettingsUpdated(event: FundSettingsUpdated): void {
   let policy = ensureNoDepegOnRedeemSharesForSpecificAssetsPolicy(event.params.comptrollerProxy, event.address, event);
-  policy.assetConfigs = event.params.assetConfigs;
+  policy.assetConfigs = event.params.assetConfigs.map<AssetConfig>((assetConfig) => ensureAssetConfig(assetConfig.asset, assetConfig.referenceAsset, assetConfig.deviationToleranceInBps, event));
   policy.updatedAt = event.block.timestamp.toI32();
   policy.save();
 }
