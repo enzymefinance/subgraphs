6c45b0333f86b826eff847f839db44550c65c317
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-04-24 14:59:33 +0200

    feat: update market registry events

--- subgraphs/enzyme-core/mappings/PendleMarketsRegistry.ts
+++ subgraphs/enzyme-core/mappings/PendleMarketsRegistry.ts
@@ -1,16 +1,12 @@
 import { useVault } from '../entities/Vault';
-import { MarketAdded, MarketRemoved } from '../generated/contracts/PendleMarketsRegistryEvents';
+// import {MarketForUserUpdated } from '../generated/contracts/PendleMarketsRegistryEvents';
 import { ensurePendleV2AllowedMarket, usePendleV2AllowedMarket } from '../entities/PendleV2Position';
 
-export function handleMarketAdded(event: MarketAdded): void {
-  let market = ensurePendleV2AllowedMarket(event.params.vaultProxy, event.params.marketAddress);
-  market.duration = event.params.duration.toI32();
-  market.active = true;
-  market.save();
-}
-
-export function handleMarketRemoved(event: MarketRemoved): void {
-  let market = usePendleV2AllowedMarket(event.params.vaultProxy, event.params.marketAddress);
-  market.active = false;
-  market.save();
+export function handleMarketForUserUpdated(): void {
+// event: MarketForUserUpdated
+  // let market = ensurePendleV2AllowedMarket(event.params.user, event.params.marketAddress);
+  // let duration = event.params.duration.toI32();
+  // market.duration = duration;
+  // market.active = duration > 0;
+  // market.save();
 }
