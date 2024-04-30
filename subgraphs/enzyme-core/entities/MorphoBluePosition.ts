e5f93839009e77f2cbf681c1c3dfe95bf3813c83
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-04-30 15:47:32 +0200

    fix: build

--- subgraphs/enzyme-core/entities/MorphoBluePosition.ts
+++ subgraphs/enzyme-core/entities/MorphoBluePosition.ts
@@ -1,5 +1,12 @@
 import { Address, Bytes, ethereum } from '@graphprotocol/graph-ts';
-import { AssetAmount, ExternalPositionType, MorphoBlueMarket, MorphoBluePosition, MorphoBluePositionChange, Vault } from '../generated/schema';
+import {
+  AssetAmount,
+  ExternalPositionType,
+  MorphoBlueMarket,
+  MorphoBluePosition,
+  MorphoBluePositionChange,
+  Vault,
+} from '../generated/schema';
 import { useVault } from './Vault';
 import { logCritical, uniqueEventId } from '@enzymefinance/subgraph-utils';
 import { getActivityCounter } from './Counter';
@@ -20,14 +27,14 @@ export function ensureMorphoBlueMarket(morphoBluePosition: Address, marketId: By
   }
 
   let contract = ExternalSdk.bind(morphoBlueAddress);
-  let marketParams = contract.try_idToMarketParams(marketId);
+  let marketParams = contract.try_idToMarketParams(Address.fromBytes(marketId));
 
   if (marketParams.reverted) {
-    logCritical('Unable to obtain market params for market id {}',[marketId.toHex()])
+    logCritical('Unable to obtain market params for market id {}', [marketId.toHex()]);
   }
 
   let loanToken = ensureAsset(marketParams.value.loanToken);
-  let collateralToken = ensureAsset(marketParams.value.collateralToken)
+  let collateralToken = ensureAsset(marketParams.value.collateralToken);
 
   market = new MorphoBlueMarket(id);
   market.marketId = marketId;
@@ -41,51 +48,50 @@ export function ensureMorphoBlueMarket(morphoBluePosition: Address, marketId: By
 }
 
 export function createMorphoBluePosition(
-    externalPositionAddress: Address,
-    vaultAddress: Address,
-    type: ExternalPositionType,
+  externalPositionAddress: Address,
+  vaultAddress: Address,
+  type: ExternalPositionType,
 ): MorphoBluePosition {
-    let position = new MorphoBluePosition(externalPositionAddress.toHex());
-    position.vault = useVault(vaultAddress.toHex()).id;
-    position.active = true;
-    position.type = type.id;
-    position.save();
-  
-    return position;
-  }
-  
-  export function useMorphoBluePosition(id: string): MorphoBluePosition {
-    let position = MorphoBluePosition.load(id);
-    if (position == null) {
-      logCritical('Failed to load MorphoBluePosition {}.', [id]);
-    }
-  
-    return position as MorphoBluePosition;
+  let position = new MorphoBluePosition(externalPositionAddress.toHex());
+  position.vault = useVault(vaultAddress.toHex()).id;
+  position.active = true;
+  position.type = type.id;
+  position.save();
+
+  return position;
+}
+
+export function useMorphoBluePosition(id: string): MorphoBluePosition {
+  let position = MorphoBluePosition.load(id);
+  if (position == null) {
+    logCritical('Failed to load MorphoBluePosition {}.', [id]);
   }
 
-  export function createMorphoBluePositionChange(
-    MorphoBluePositionAddress: Address,
-    changeType: string,
-    vault: Vault,
-    market: MorphoBlueMarket,
-    assetAmount: AssetAmount,
-    event: ethereum.Event,
-  ): MorphoBluePositionChange {
-    let change = new MorphoBluePositionChange(uniqueEventId(event));
-    change.morphoBluePositionChangeType = changeType;
-    change.externalPosition = MorphoBluePositionAddress.toHex();
-    change.vault = vault.id;
-    change.timestamp = event.block.timestamp.toI32();
-    change.market = market.id;
-    change.assetAmount = assetAmount.id;
-    change.activityCounter = getActivityCounter();
-    change.activityCategories = ['Vault'];
-    change.activityType = 'Trade';
-    change.save();
-  
-    vault.lastAssetUpdate = event.block.timestamp.toI32();
-    vault.save();
-  
-    return change;
-  }
-  
+  return position as MorphoBluePosition;
+}
+
+export function createMorphoBluePositionChange(
+  MorphoBluePositionAddress: Address,
+  changeType: string,
+  vault: Vault,
+  market: MorphoBlueMarket,
+  assetAmount: AssetAmount,
+  event: ethereum.Event,
+): MorphoBluePositionChange {
+  let change = new MorphoBluePositionChange(uniqueEventId(event));
+  change.morphoBluePositionChangeType = changeType;
+  change.externalPosition = MorphoBluePositionAddress.toHex();
+  change.vault = vault.id;
+  change.timestamp = event.block.timestamp.toI32();
+  change.market = market.id;
+  change.assetAmount = assetAmount.id;
+  change.activityCounter = getActivityCounter();
+  change.activityCategories = ['Vault'];
+  change.activityType = 'Trade';
+  change.save();
+
+  vault.lastAssetUpdate = event.block.timestamp.toI32();
+  vault.save();
+
+  return change;
+}
