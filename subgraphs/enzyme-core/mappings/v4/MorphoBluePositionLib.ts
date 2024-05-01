9d49714751de2d7317d90ec268348347010c08ab
Ivan Herger <36133712+iherger@users.noreply.github.com>
2024-05-01 09:48:14 +0200

    fix: morpho blue (#89)

--- subgraphs/enzyme-core/mappings/v4/MorphoBluePositionLib.ts
+++ subgraphs/enzyme-core/mappings/v4/MorphoBluePositionLib.ts
@@ -2,12 +2,12 @@ import { ensureMorphoBlueMarket } from '../../entities/MorphoBluePosition';
 import { MarketIdAdded, MarketIdRemoved } from '../../generated/contracts/MorphoBluePositionLib4Events';
 
 export function handleMarketIdAdded(event: MarketIdAdded): void {
-  let market = ensureMorphoBlueMarket(event.params.marketId, event.address);
+  let market = ensureMorphoBlueMarket(event.address, event.params.marketId);
   market.removed = false;
   market.save();
 }
 export function handleMarketIdRemoved(event: MarketIdRemoved): void {
-  let market = ensureMorphoBlueMarket(event.params.marketId, event.address);
+  let market = ensureMorphoBlueMarket(event.address, event.params.marketId);
   market.removed = true;
   market.save();
 }
