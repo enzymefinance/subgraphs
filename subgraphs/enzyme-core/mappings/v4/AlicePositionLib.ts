3b22e03aeed4be1261722646dd7b377830cf35fd
Ivan Herger <ivan@herger.org>
2024-08-12 12:09:32 +0200

    fix: handle ETH assets

--- subgraphs/enzyme-core/mappings/v4/AlicePositionLib.ts
+++ subgraphs/enzyme-core/mappings/v4/AlicePositionLib.ts
@@ -4,9 +4,10 @@ import { AliceOrder } from '../../generated/schema';
 import { useAliceOrder, useAlicePosition } from '../../entities/AlicePosition';
 import { ensureAsset } from '../../entities/Asset';
 import { createAssetAmount } from '../../entities/AssetAmount';
-import { toBigDecimal } from '@enzymefinance/subgraph-utils';
+import { toBigDecimal, ZERO_ADDRESS } from '@enzymefinance/subgraph-utils';
 import { useVault } from '../../entities/Vault';
 import { ensureComptroller } from '../../entities/Comptroller';
+import { wethTokenAddress } from '../../generated/addresses';
 
 export function handleOrderIdAdded(event: OrderIdAdded): void {
   let position = useAlicePosition(event.address.toHex());
@@ -14,7 +15,8 @@ export function handleOrderIdAdded(event: OrderIdAdded): void {
   let comptroller = ensureComptroller(Address.fromString(vault.comptroller), event);
   let denominationAsset = ensureAsset(Address.fromString(comptroller.denomination));
 
-  let outgoingAsset = ensureAsset(event.params.orderDetails.outgoingAssetAddress);
+  let outgoingAssetAddress = event.params.orderDetails.outgoingAssetAddress.equals(ZERO_ADDRESS) ? wethTokenAddress : event.params.orderDetails.outgoingAssetAddress;
+  let outgoingAsset = ensureAsset(outgoingAssetAddress);
   let outgoingAssetAmount = createAssetAmount(
     outgoingAsset,
     toBigDecimal(event.params.orderDetails.outgoingAmount, outgoingAsset.decimals),
@@ -23,7 +25,8 @@ export function handleOrderIdAdded(event: OrderIdAdded): void {
     event,
   );
 
-  let incomingAsset = ensureAsset(event.params.orderDetails.incomingAssetAddress);
+  let incomingAssetAddress = event.params.orderDetails.incomingAssetAddress.equals(ZERO_ADDRESS) ? wethTokenAddress : event.params.orderDetails.incomingAssetAddress;
+  let incomingAsset = ensureAsset(incomingAssetAddress);
 
   let order = new AliceOrder(event.params.orderId.toString());
   order.createdAt = event.block.timestamp.toI32();
