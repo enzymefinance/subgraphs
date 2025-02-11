74a7f6788d759d3338eaec130f68eaa1d6e771c5
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2025-02-11 14:41:03 +0100

    feat: uncomment the graph withdraw on arbitrum (#122)

--- subgraphs/enzyme-core/mappings/v4/ExternalPositionManager.ts
+++ subgraphs/enzyme-core/mappings/v4/ExternalPositionManager.ts
@@ -1387,11 +1387,6 @@ export function handleCallOnExternalPositionExecutedForFund(event: CallOnExterna
     }
 
     if (actionId == TheGraphDelegationPositionActionId.Withdraw) {
-      // Returning early on Arbitrum until this has been tested
-      if (dataSource.network() == 'arbitrum-one') {
-        return;
-      }
-
       let decoded = ethereum.decode('(address,address)', event.params.actionArgs);
 
       if (decoded == null) {
